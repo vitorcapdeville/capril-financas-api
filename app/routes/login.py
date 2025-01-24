3e2e7813bfb847cf141e9892833762ec47fe2b23
Vitor Capdeville <vgcapdeville@hotmail.com>
2025-01-24 11:46:34 -0300

    🚑️ Traduzir mensagens de erro na rota de login.

--- app/routes/login.py
+++ app/routes/login.py
@@ -20,9 +20,9 @@ def login_access_token(session: SessionDep, form_data: Annotated[OAuth2PasswordR
     """
     user = crud.authenticate(session=session, email=form_data.username, password=form_data.password)
     if not user:
-        raise HTTPException(status_code=400, detail="Incorrect email or password")
+        raise HTTPException(status_code=400, detail="Email ou senha incorretos.")
     elif not user.is_active:
-        raise HTTPException(status_code=400, detail="Inactive user")
+        raise HTTPException(status_code=400, detail="Usuário inativo.")
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     return Token(access_token=security.create_access_token(user.id, expires_delta=access_token_expires))
 
